6eecd813f9119813da2446bc0fda21a6aaf301f7
ProfBlack <55605783+ProfBlack@users.noreply.github.com>
2025-05-06 09:41:42 +0100

    Slower taller

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Configuration Section
 const config = {
-    chatSpeedMin: 60, // Minimum chat message interval in milliseconds (1 second)
-    chatSpeedMax: 500, // Maximum chat message interval in milliseconds (4 seconds)
+    chatSpeedMin: 90, // Minimum chat message interval in milliseconds (1 second)
+    chatSpeedMax: 700, // Maximum chat message interval in milliseconds (4 seconds)
     maxMessagesDisplayed: 100, // Maximum number of messages displayed in the chat box before clearing
     chatBoxSelector: '#chat-box', // Chat box element selector
     usernamesInputSelector: '#usernames-input', // Usernames input selector
